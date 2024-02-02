1051cd339e7854bfc97da7773b1fa5794411517d
PythonDecorator <okpeamos.ao@gmail.com>
2024-02-02 20:20:14 +0100

    Added gunicorn

--- app/settings.py
+++ app/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-KEY = 'django-insecure-+l5fxl*2nus4_l0*b4)kzl6d3+#i$+sa6^bzsa1^fw3$gf415s'
-SECRET_KEY = os.environ.get('SECRET_KEY', KEY)
+SECRET_KEY =  'django-insecure-+l5fxl*2nus4_l0*b4)kzl6d3+#i$+sa6^bzsa1^fw3$gf415s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 1)))
+DEBUG = False
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -164,9 +167,9 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React app URL
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # React app URL
+# ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
