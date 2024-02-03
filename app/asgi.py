43fc90d1b32ede4a516c6a7093da12cfadc58495
PythonDecorator <okpeamos.ao@gmail.com>
2024-02-03 07:51:25 +0100

    Added djangosetup

--- app/asgi.py
+++ app/asgi.py
@@ -1,13 +1,17 @@
 import os
 
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 django_asgi_app = get_asgi_application()
 
